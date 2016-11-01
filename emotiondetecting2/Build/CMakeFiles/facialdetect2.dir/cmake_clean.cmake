179b8d514159db99457f321e4c0682eafc1b3d7a
MinjiEstelle <minjiestelle@gmail.com>
2016-11-01 19:29:52 +0000

    add facial detection codes

---
+++ emotiondetecting2/Build/CMakeFiles/facialdetect2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/facialdetect2.dir/facialdetect2.cpp.o"
+  "facialdetect2.pdb"
+  "facialdetect2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/facialdetect2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
